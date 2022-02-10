87754e7d68ab9fcc4e28610deccf439c0e4927ae
Prithvi Kohli <69433082+prithvikohli@users.noreply.github.com>
2022-02-10 02:13:00 +0000

    implementing full CNN model

--- experiments/shot-recognition/motion_to_image.py
+++ experiments/shot-recognition/motion_to_image.py
@@ -19,33 +19,34 @@ def motion_to_image(img_path, out_path):
             coords = [float(c) for c in line.split(" ")]
             image[-1].append(coords)
 
+    motion_image = image.copy()
     for t in range(len(image)-1):
         for n in range(len(image[t])):
-            image[t][n][0] = image[t+1][n][0] - image[t][n][0]
-            image[t][n][1] = image[t+1][n][1] - image[t][n][1]
-            image[t][n][2] = image[t+1][n][2] - image[t][n][2]
-    image.pop()
+            motion_image[t][n][0] = image[t+1][n][0] - image[t][n][0]
+            motion_image[t][n][1] = image[t+1][n][1] - image[t][n][1]
+            motion_image[t][n][2] = image[t+1][n][2] - image[t][n][2]
+    motion_image.pop()
 
-    image = np.array(image)
-    image /= image.max()    # normalise to [-1,1]
-    cv2.imwrite(out_path, image)
+    motion_image = np.array(motion_image)
+    motion_image /= np.abs(motion_image).max()    # normalise to [-1,1]
+    cv2.imwrite(out_path, (motion_image+1)*127.5)  # fit image data to [0,255]
     
 
-def process_motions(root, out_dir):
+def process_motions(root, out_dir, _type):
     if not isdir(out_dir):
         mkdir(out_dir)
     
     skeleton_paths = [f for f in listdir(root) if isfile(join(root, f))]
     for s in skeleton_paths:
-        motion_to_image(root + s, out_dir + s[:-4] + ".png")
+        motion_to_image(root + s, out_dir + _type + "_" + s[:-4] + ".png")
 
 if __name__ == "__main__":
     amateur_root = "data/THETIS_Skeletal_Joints/normal_oniFiles/ONI_AMATEURS/"
     shot_paths = [d for d in listdir(amateur_root) if isdir(join(amateur_root, d))]
     for s in shot_paths:
-        process_motions(amateur_root + s + "/", "data/motion_images/amateur_" + s + "/")
+        process_motions(amateur_root + s + "/", "data/motion_images/" + s + "/", "amateur")
 
     expert_root = "data/THETIS_Skeletal_Joints/normal_oniFiles/ONI_EXPERTS/"
     shot_paths = [d for d in listdir(expert_root) if isdir(join(expert_root, d))]
     for s in shot_paths:
-        process_motions(expert_root + s + "/", "data/motion_images/expert_" + s + "/")
+        process_motions(expert_root + s + "/", "data/motion_images/" + s + "/", "expert")
--- experiments/shot-recognition/skeleton_to_image.py
+++ experiments/shot-recognition/skeleton_to_image.py
@@ -24,22 +24,22 @@ def skeleton_to_image(img_path, out_path):
     cv2.imwrite(out_path, (image+1)*127.5)  # fit image data to [0,255]
     
 
-def process_skeletons(root, out_dir):
+def process_skeletons(root, out_dir, _type):
     if not isdir(out_dir):
         mkdir(out_dir)
     
     skeleton_paths = [f for f in listdir(root) if isfile(join(root, f))]
     for s in skeleton_paths:
-        skeleton_to_image(root + s, out_dir + s[:-4] + ".png")
+        skeleton_to_image(root + s, out_dir + _type + "_" + s[:-4] + ".png")
 
 if __name__ == "__main__":
     amateur_root = "data/THETIS_Skeletal_Joints/normal_oniFiles/ONI_AMATEURS/"
     shot_paths = [d for d in listdir(amateur_root) if isdir(join(amateur_root, d))]
     for s in shot_paths:
-        process_skeletons(amateur_root + s + "/", "data/skeleton_images/amateur_" + s + "/")
+        process_skeletons(amateur_root + s + "/", "data/skeleton_images/" + s + "/", "amateur")
 
     expert_root = "data/THETIS_Skeletal_Joints/normal_oniFiles/ONI_EXPERTS/"
     shot_paths = [d for d in listdir(expert_root) if isdir(join(expert_root, d))]
     for s in shot_paths:
-        process_skeletons(expert_root + s + "/", "data/skeleton_images/expert_" + s + "/")
+        process_skeletons(expert_root + s + "/", "data/skeleton_images/" + s + "/", "expert")
 

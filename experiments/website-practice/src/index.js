05383afc7ac0aaa0efe9d5f5dd4242a8b031e9b4
Charlie <72133888+Catcatcher33@users.noreply.github.com>
2022-02-17 12:40:46 +0000

    Got loading working - can see model's silhouette

--- experiments/website-practice/src/index.js
+++ experiments/website-practice/src/index.js
@@ -3,68 +3,56 @@ import * as THREE from 'three';
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 let scene, renderer, camera;
-let model;
 
 // Initialisation, based on https://github.com/mrdoob/three.js/blob/master/examples/webgl_animation_skinning_blending.html
+// Guide used. https://sbcode.net/threejs/loaders-gltf/
 
+// init container
 const container = document.getElementById('container');
 document.body.appendChild(container);
 
-camera = new THREE.PerspectiveCamera(45, window.innerHeight / window.innerHeight, 1, 1000);
-camera.position.set(1,2,-3);
-camera.lookAt(0,1,0);
-
-scene = new THREE.Scene();
-scene.background = new THREE.Color(0xfb000);
-
-const loader = new GLTFLoader();
-loader.load('Soldier.glb', function(gltf){
-    model = gltf.scene;
-    scene.add(model);
-
-}, 
-undefined, 
-function(e){
-    console.error(e);
-});
-
-renderer = new THREE.WebGLRenderer({antialias:true});
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.outputEncoding = THREE.sRGBEncoding;
-//renderer.shadowMap.enabled = true;
-container.appendChild(renderer.domElement);
-renderer.render(scene, camera);
-
-/*
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-
-const loader = new GLTFLoader();
-
-loader.load(
-    'scifi_girl_v.01/scene.gltf',
-    function(gltf) {
-        scene.add(gltf.scene);
-        gltf.animations;
-        gltf.scene;
-        gltf.scenes;
-        gltf.cameras;
-        gltf.asset;
-    },
-    function (xhr) {
-        console.log((xhr.loaded/xhr.total*100) + '% loaded');
-    },
-    function (error){
-        console.log(error);
-    }
+// init camera
+camera = new THREE.PerspectiveCamera(
+    75, 
+    window.innerWidth / window.innerHeight, 
+    0.1, 
+    1000
 );
+camera.position.z = 2;
+camera.position.y = 1;
 
-camera.position.z = 5;
-renderer.render( scene, camera );*/
+// init renderer
+renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+container.appendChild(renderer.domElement);
+
+// init scene
+scene = new THREE.Scene();
+scene.add(new THREE.AxesHelper(5));
+scene.background = new THREE.Color(0xfe000);
+
+// init loader
+const modelLoader = new GLTFLoader();
+modelLoader.load(
+    'Soldier.glb',
+    function(gltf){
+    scene.add(gltf.scene);
+    },
+    undefined,
+    function error(e){
+        console.error(e);
+    });
+
+window.addEventListener('resize', onWindowResize, false);
+function onWindowResize(){
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    render();
+}
+
+function render(){
+    renderer.render(scene, camera);
+}
+
+render();

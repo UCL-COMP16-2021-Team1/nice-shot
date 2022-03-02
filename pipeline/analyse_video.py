84ae467d14bb0a8da31e1adca896040bae3480ac
Prithvi Kohli <69433082+prithvikohli@users.noreply.github.com>
2022-03-02 03:43:26 +0000

    JSON keyframes and timestamps data

--- pipeline/analyse_video.py
+++ pipeline/analyse_video.py
@@ -5,35 +5,41 @@ from extract_pose import extract_pose_frames
 from detect_shot_times import detect_shot_times
 from classify_shot import classify_shot
 
-# pipeline entry point
 def analyse_video(cap):
+    fps = cap.get(cv2.CAP_PROP_FPS)
     frames = []
+    timestamps = []
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
         frames.append(frame)
-    cap.release()
+        timestamps.append(cap.get(cv2.CAP_PROP_POS_MSEC))
     frames = np.stack(frames)
 
     world_pose_frames, image_pose_frames = extract_pose_frames(frames)
     shot_times = detect_shot_times(world_pose_frames[:,3,...], world_pose_frames[:,10,...])
 
-    shot_intervals = []
-    shot_classifications = []
-    shot_world_poses = []
-    shot_image_poses = []
+    shot_analysis = {}
+    shot_analysis["intervals"] = []
+    shot_analysis["timestamps"] = []
+    shot_analysis["classifications"] = []
+    shot_analysis["world_poses"] = []
+    shot_analysis["image_poses"] = []
     for t in shot_times:
-        # assume shot takes 2 seconds for a 30fps video
-        start_t = max(0,t-30)
-        end_t = min(t+31,len(world_pose_frames)-1)
-        shot_intervals.append((start_t, end_t))
+        # assume shot takes 2 seconds
+        # TODO: make shot clipping more precise?
+        interval = 2 * fps
+        start_t = max(0,int(t-interval))
+        end_t = min(int(t+interval)+1, len(world_pose_frames)-1)
+        shot_analysis["intervals"].append((start_t, end_t))
+        shot_analysis["timestamps"].append(timestamps[start_t:end_t])
 
         world_pose = world_pose_frames[start_t:end_t]
-        shot_world_poses.append(world_pose)
+        shot_analysis["world_poses"].append(world_pose)
         image_pose = image_pose_frames[start_t:end_t]
-        shot_image_poses.append(image_pose)
+        shot_analysis["image_poses"].append(image_pose)
         shot = classify_shot(world_pose)[0]
-        shot_classifications.append(shot)
+        shot_analysis["classifications"].append(shot)
     
-    return shot_intervals, shot_classifications, shot_world_poses, shot_image_poses
+    return shot_analysis

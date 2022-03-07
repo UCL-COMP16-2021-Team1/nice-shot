fd7321c773ce6e1364c0675cc435b426763f3031
Charlie <72133888+Catcatcher33@users.noreply.github.com>
2022-03-07 15:15:50 +0000

    cleaned up directories. Doesn't necessarily work yet

--- misc/sandbox/website-practice/src/index.js
+++ misc/sandbox/website-practice/src/index.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 let scene, renderer, camera;

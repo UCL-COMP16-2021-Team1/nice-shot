c37e6863e7b4c37ca9e93ce59df593a12714465b
Charlie <72133888+Catcatcher33@users.noreply.github.com>
2022-03-07 15:12:06 +0000

    Successfully shows 2nd webbrowser (shot dashboard)

--- src/prototype/ui/downloadJSON.js
+++
@@ -1,7 +0,0 @@
-let shots, shotCount;
-$.getJSON("data/test_shot.json", function (data) {
-    shots = data.shots;
-    shotCount = shots.length;
-}).fail(function () {
-    console.log("The JSON file could not be loaded.");
-});

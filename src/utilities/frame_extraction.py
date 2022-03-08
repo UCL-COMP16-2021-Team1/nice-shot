9e10413ae83a2d4097005d262102bf26ad13d88a
Charlie <72133888+Catcatcher33@users.noreply.github.com>
2022-03-08 02:51:43 +0000

    working on webapp, not done yet
    saving changes

--- src/utilities/frame_extraction.py
+++ src/utilities/frame_extraction.py
@@ -3,14 +3,15 @@ import json
 
 # create data folder.
 
+
 def create_images():
-    with open('data/test_shot.json') as file:
+    with open('data/analysis_results/test_shot.json') as file:
         data = json.load(file)
 
         current_frame: int = 0
-        shotCount: int = 0;
+        shotCount: int = 0
 
-        cam = cv2.VideoCapture("data/p1_backhand_s1.mp4") # get directory
+        cam = cv2.VideoCapture("data/videos/p1_backhand_s1.mp4") # get directory
         print(cam.isOpened())
         
         for shot in data['shots']:
@@ -18,7 +19,7 @@ def create_images():
                 _, frame = cam.read()
                 
                 if current_frame == shot['start_frame_idx']:
-                    imgName = "data/shot" + str(shotCount) + '.jpg'
+                    imgName = "data/shot_thumbnails/shot" + str(shotCount) + '.jpg'
                     cv2.imwrite(imgName, frame)
                     current_frame += 1
                     shotCount += 1

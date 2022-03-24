561be3de52a05565681ad378a3c2ca38dd3d78b3
Charlie <72133888+Catcatcher33@users.noreply.github.com>
2022-03-24 21:58:10 +0000

    Removed unnecessary files left in repo

--- src/utilities/constants.py
+++ src/utilities/constants.py
@@ -4,9 +4,5 @@ file_path: str = ''
 json_path: str = ''
 
 ANALYSIS_FOLDER: str = 'static/analysis_results'
-SHOT_IMAGES_FOLDER: str = 'static/images'
-UPLOAD_FOLDER: str = 'static/uploads'
-STATIC_VIDEOS: str = 'static/media/videos'
-STATIC_IMAGES: str = 'static/media/images'
 
 ALLOWED_EXTENSIONS: set = {'mp4'}
